dda16f17c3f2929ff284c4c7f3ea8e072c0e647b
Aboobacker MK <aboobackervyd@gmail.com>
2024-09-28 17:01:05 +0530

    chore: add linux dir

--- lib/charlock_holmes.rb
+++ lib/charlock_holmes.rb
@@ -18,7 +18,7 @@ module CharlockHolmes
     if ENV["CharlockHolmes_DIR"].nil?
       case RbConfig::CONFIG["host_os"]
       when /darwin/
-        ["/opt/homebrew/opt/icu4c/lib", "/usr/local/lib"].find { |path| Dir.exist?(path) }
+        ["/opt/homebrew/opt/icu4c/lib", "/usr/local/lib", "/usr/lib/x86_64-linux-gnu"].find { |path| Dir.exist?(path) }
       when /linux/
         "/usr/local/lib" # Adjust if CharlockHolmes is installed elsewhere
       else
